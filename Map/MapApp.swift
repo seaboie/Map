558233e6eca1286361c1c9c6b9df072aa4bd4e65
kritbovorn <kritbovorn@gmail.com>
2021-12-31 19:16:53 +0700

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Kritbovorn Taweeyossak on 31/12/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
